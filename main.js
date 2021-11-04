3a0b8d617a592c8e6cf92b170b9324dbef31c0a3
ashimsharma337 <you@example.com>
2021-11-04 22:21:48 +0545

    logic added for addition of students object from array to table

--- main.js
+++ main.js
@@ -16,3 +16,44 @@ newCell2.innerHTML = position.value;
 newCell3.innerHTML = salary.value;
 
 }
+
+let studentsDetail = [
+    {
+    firstName: "john",
+    lastName: "lee",
+    email: "123@email.com",
+    age: 27
+    },
+    {
+    firstName: "lisa",
+    lastName: "stefen",
+    email: "333@email.com",
+    age: 27
+    },
+    {
+    firstName: "mary",
+    lastName: "johnson",
+    email: "243@email.com",
+    age: 27
+    },
+    {
+    firstName: "patrik",
+    lastName: "winget",
+    email: "566@email.com",
+    age: 27    
+    }
+];
+
+function currentStudents() {
+var tableBody = "<tbody>";
+for (let i = 0; i < studentsDetail.length; i++) {
+    tableBody += "<tr>";
+    tableBody += "<td>" + studentsDetail[i].firstName + "</td>";
+    tableBody += "<td>" + studentsDetail[i].lastName + "</td>";
+    tableBody += "<td>" + studentsDetail[i].email + "</td>";
+    tableBody += "<td>" + studentsDetail[i].age + "</td>";
+}
+tableBody += "</tbody>";
+
+document.getElementById("tableData").innerHTML = tableBody;
+}
